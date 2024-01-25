303adf52591272c5b998a3aeac5355b71751e93c
redducc <sushmit718@gmail.com>
2024-01-25 15:05:13 +0100

    agm: Restrict to 5.10

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 ifeq ($(strip $(TARGET_USES_QCOM_MM_AUDIO)),true)
+ifeq ($(TARGET_KERNEL_VERSION),5.10)
 ifneq ($(wildcard vendor/qcom/proprietary/args),)
 	include $(call all-subdir-makefiles)
+endif
 endif
 endif
